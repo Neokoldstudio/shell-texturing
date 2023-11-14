92f1e5a6b7c0e125ab3ea5be44ea917c7cab5691
Neokoldstudio <nightmare.vid@gmail.com>
2023-11-14 12:16:21 -0500

    some updates

--- instanciateShell.gd
+++ instanciateShell.gd
@@ -1,17 +1,12 @@
 extends Node
 
 var _ShellCount = 16;
-var _Layers = []
+var scene = preload("res://shells.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var scene = preload("res://shells.tscn")
-
+	
 	for i in range(_ShellCount):
 		var instance = scene.instantiate();
 		add_child(instance);
 		print("shell created");
-		
-
-	for i in _Layers:
-		pass

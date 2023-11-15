7705084d92fc675bfc2f130ee3d83fa15411789a
Neokoldstudio <nightmare.vid@gmail.com>
2023-11-15 11:11:15 -0500

    shells are now instanced with the correct material parameters

--- instanciateShell.gd
+++ instanciateShell.gd
@@ -1,3 +1,4 @@
+@tool
 extends Node
 
 var _ShellCount = 16;
@@ -5,8 +6,11 @@ var scene = preload("res://shells.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	
 	for i in range(_ShellCount):
 		var instance = scene.instantiate();
+		instance.mesh.material.set_shader_parameter("_Dentsity", 100.0);
+		instance.mesh.material.set_shader_parameter("_ShellIndex", i);
+		instance.mesh.material.set_shader_parameter("_ShellCount", _ShellCount);
+		instance.mesh.material.set_shader_parameter("_ShellLength", 1);
 		add_child(instance);
 		print("shell created");

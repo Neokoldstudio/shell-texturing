4dae4b0a1919b98f0c07eae1df75585940879ce4
Neokoldstudio <nightmare.vid@gmail.com>
2023-11-17 14:44:49 -0500

    THE MATH IS MATHING

--- instanciateShell.gd
+++ instanciateShell.gd
@@ -1,16 +1,19 @@
-@tool
 extends Node
 
 var _ShellCount = 16;
 var scene = preload("res://shells.tscn")
+var density = 100.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	for i in range(_ShellCount):
 		var instance = scene.instantiate();
-		instance.mesh.material.set_shader_parameter("_Dentsity", 100.0);
-		instance.mesh.material.set_shader_parameter("_ShellIndex", i);
-		instance.mesh.material.set_shader_parameter("_ShellCount", _ShellCount);
-		instance.mesh.material.set_shader_parameter("_ShellLength", 1);
 		add_child(instance);
 		print("shell created");
+		
+	#for i in range(get_child_count()):
+		#var child = get_child(i)
+		#child.mesh.material.sets_shader_parameter("_Density", density);
+		#child.mesh.material.set_shader_parameter("_ShellIndex", i);
+		#child.mesh.material.set_shader_parameter("_ShellCount", _ShellCount);
+		#child.mesh.material.set_shader_parameter("_ShellLength", 1);

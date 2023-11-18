1332cefd61ed419c049f976fcaa7684040083575
Neokoldstudio <nightmare.vid@gmail.com>
2023-11-18 00:33:03 -0500

    the shader is functional, now lets make the math right and make it pretty

--- instanciateShell.gd
+++ instanciateShell.gd
@@ -1,19 +1,25 @@
+@tool
 extends Node
 
 var _ShellCount = 16;
-var scene = preload("res://shells.tscn")
+var _ShellLength = 1
 var density = 100.0
-
+var scene = preload("res://shells.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	for i in range(_ShellCount):
-		var instance = scene.instantiate();
-		add_child(instance);
-		print("shell created");
+		createShell(i);
+		await get_tree().create_timer(0.05).timeout
 		
-	#for i in range(get_child_count()):
-		#var child = get_child(i)
-		#child.mesh.material.sets_shader_parameter("_Density", density);
-		#child.mesh.material.set_shader_parameter("_ShellIndex", i);
-		#child.mesh.material.set_shader_parameter("_ShellCount", _ShellCount);
-		#child.mesh.material.set_shader_parameter("_ShellLength", 1);
+		
+func createShell(index:int):
+	var instance = scene.instantiate();
+	add_child(instance);
+	var material_instance = instance.get_surface_override_material(0).duplicate();
+	instance.set_surface_override_material(0, material_instance);
+	material_instance.set_shader_parameter("_Density", density);
+	material_instance.set_shader_parameter("_ShellIndex", index);
+	material_instance.set_shader_parameter("_ShellCount", _ShellCount);
+	material_instance.set_shader_parameter("_ShellLength", 0.1);
+	
+	print("shell created");
